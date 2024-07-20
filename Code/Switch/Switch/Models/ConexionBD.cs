8813fb3d7c0d9a41a3095382a90ef400220c743c
scuareniga <steavenmoreno@gmail.com>
2024-07-19 22:29:47 -0500

    se adicionan los modelos al codigo

---
+++ Code/Switch/Switch/Models/ConexionBD.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Switch.Models
+{
+    public class ConexionBD
+    {
+        private string server = "localhost";
+        private string database = "switch";
+        private string user = "root";
+        private string password = "";
+    }
+}
---
+++ Code/Switch/Switch/Models/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Switch.Models
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+        public string Correo { get; set; }
+        public string Clave { get; set; }
+        public string ConfirmarClave { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Genero { get; set; }
+        public Datetime Fecha_Nacimiento { get; set; }
+        public string Celular { get; set; }
+        public string Correo_Electronico { get; set; }
+        public string Links_RS { get; set; }
+        public string Habilidades { get; set; }
+    }
+}
