d0440b8807099312cf0931b473fe1b1c67867c7d
Julian Yesith Moreno <Julianyesithmoreno@gmail.com>
2024-07-29 16:07:01 -0500

    Actualice bundler

--- Code/Switch/Switch/App_Start/BundleConfig.cs
+++ Code/Switch/Switch/App_Start/BundleConfig.cs
@@ -8,8 +8,8 @@ namespace Switch
         // Para obtener más información sobre las uniones, visite https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/complementos").Include(
+                        "~/Scripts/scripts.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
