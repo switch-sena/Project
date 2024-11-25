79a4e6a9c33a13837493bdd56e5f556cd1278303
Santy <Santy@DESKTOP-PIRH9NL>
2024-11-24 22:45:42 -0500

    Se hace la creacion de los modelos y el controlador para la nueva API

---
+++ Code/ConsumeSwitch/ConsumeMicroservices/Controllers/PublicacionesDTOController.cs
@@ -0,0 +1,50 @@
+﻿using ConsumeMicroservices.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+namespace ConsumeMicroservices.Controllers
+{
+    public class PublicacionesDTOController : Controller
+    {
+        string apiUrl = ConfigurationManager.AppSettings["Api"].ToString();
+        string bearerToken = string.Empty;
+
+        //Get
+        public async Task<ActionResult> Index()
+        {
+            if (!string.IsNullOrEmpty(Session["BearerToken"].ToString()))
+            {
+                bearerToken = Session["bearerToken"] as string;
+            }
+            else
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            List<PublicacionesDTO> EmpInfo = new List<PublicacionesDTO>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(apiUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", bearerToken);
+                HttpResponseMessage Res = await client.GetAsync("api/Publicaciones/GetPublicacionesInfoComp");
+                if (Res.IsSuccessStatusCode)
+                {
+                    var EmpResponse = Res.Content.ReadAsStringAsync().Result;
+                    EmpInfo = JsonConvert.DeserializeObject<List<PublicacionesDTO>>(EmpResponse);
+                }
+            }
+
+            return View(EmpInfo);
+        }
+
+    }
+}
---
+++ Code/ConsumeSwitch/ConsumeMicroservices/Models/PublicacionesDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConsumeMicroservices.Models
+{
+    public class PublicacionesDTO
+    {
+        public int IdPubl { get; set; }
+        public string TituloPubl { get; set; }
+        public string DescripcionPubl { get; set; }
+        public string NombreUsuario { get; set; }
+        public List<string> Habilidades { get; set; }
+        public List<string> Modalidades { get; set; }
+    }
+}
